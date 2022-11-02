512fab8b104ec597db8404c80aa85f3b53774432
roger067 <rosestolato.rogerio@gmail.com>
2022-11-02 04:10:07 -0300

    add error prop to input

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -5,18 +5,25 @@ import { COLORS } from ".";
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
   label: string;
+  errorMessage?: string;
 }
 
-const Input: React.FC<InputProps> = ({ label, name, ...props }) => {
+const Input: React.FC<InputProps> = ({
+  label,
+  name,
+  errorMessage,
+  ...props
+}) => {
   return (
-    <InputGroup>
+    <InputGroup hasError={!!errorMessage}>
       <input {...props} id={name} name={name} required />
       <label htmlFor={name}>{label}</label>
+      <small>{errorMessage}</small>
     </InputGroup>
   );
 };
 
-const InputGroup = styled.div`
+const InputGroup = styled.div<{ hasError: boolean }>`
   width: 100%;
   position: relative;
   margin-bottom: 62px;
@@ -27,7 +34,10 @@ const InputGroup = styled.div`
     font-size: 16px;
     color: ${COLORS.GREY_700};
     border: none;
-    border-bottom: 1px solid ${COLORS.GREY_400};
+    border-bottom: ${({ hasError }) =>
+      hasError
+        ? `1.5px solid ${COLORS.RED_500}`
+        : `1px solid ${COLORS.GREY_400}`};
     outline: none;
     background: transparent;
   }
@@ -50,6 +60,16 @@ const InputGroup = styled.div`
   input:valid ~ label {
     top: -24px;
     font-size: 12px;
+  }
+
+  small {
+    position: absolute;
+    left: 0;
+    top: 44.5px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 14.4px;
+    color ${COLORS.RED_500};
   }
 `;
 
